b0be96522afa8a9e3a74cfe480b40c3973da74d9
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-08-31 00:32:05 +0200

    Initialize recipes model, view and controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,14 @@
 class RecipesController < ApplicationController
-  def public_recipes; end
+  def index
+    @user = User.find(params[:user_id])
+    @recipes = @user.recipes.includes(:user)
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def public_recipes
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc)
+  end
 end
