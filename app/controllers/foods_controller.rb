4c8470c030ac89f90495c98da58cc52bd8779491
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-30 22:57:48 -0500

    Add create to food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -10,4 +10,22 @@ class FoodsController < ApplicationController
       format.html { render :new, locals: { food: } }
     end
   end
+
+  def create
+    @client = current_user
+    allparams = params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+    allparams['user_id'] = @client.id
+    food = Food.new(allparams)
+    respond_to do |format|
+      format.html do
+        if food.save
+          flash[:success] = 'Food added successfully!'
+          redirect_to user_foods_path(user_id: @client.id)
+        else
+          flash.now[:error] = 'Error: Food could not be added!'
+          render :new, locals: { food: }
+        end
+      end
+    end
+  end
 end
