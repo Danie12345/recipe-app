5b1b8dbd3c7e7ec7ba141be3d6691f8eec946cbe
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-30 22:55:57 -0500

    Add new to food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,12 @@ class FoodsController < ApplicationController
   def index 
     @foods = Food.all
   end
+
+  def new
+    @client = current_user
+    food = Food.new
+    respond_to do |format|
+      format.html { render :new, locals: { food: } }
+    end
+  end
 end
