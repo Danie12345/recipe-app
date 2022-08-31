f4350ee5c5e5ddea029282df3c6b74632e1c38bb
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-31 12:56:05 -0500

    Fix general shopping list url

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :users do
     resources :recipes, only: [:index, :show, :new, :create, :destroy, :public_recipes]
     resources :foods, only: [:index, :new, :create, :destroy]
-    get "/general_shopping_list", to: "foods#show"
   end
+  get "/general_shopping_list", to: "foods#list"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
