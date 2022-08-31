d3a4afeda759b128816d81b90cb7f9b7fffe5aa9
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-30 20:16:33 -0500

    Add validations to foods model

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,6 +3,12 @@ class Food < ApplicationRecord
   has_many :recipes_foods
   has_many :recipes :through => :recipes_foods
 
+  validates :name, length: { maximum: 32 }, allow_blank: false
+  validates :measurement_unit, length: { maximum: 8 }, allow_blank: false
+  validates :price, numericality: true, allow_blank: false
+  validates :quantity, numericality: true, allow_blank: false
+  validates :user_id, uniqueness: { scope: :user_id }
+
   def allrecipesfoods
     recipes_foods.order(created_at: :desc)
   end
