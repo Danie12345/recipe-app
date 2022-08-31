24197bdc0ec97fdcf6792a6a00aa140fa332e141
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-30 23:00:09 -0500

    Add custom show route
    general shopping list will correspond to the show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users do
     resources :recipes, only: [:index, :show, :new, :create, :destroy, :public_recipes]
-    resources :foods, only: [:index, :new, :create, :destroy, :general_shopping_list]
+    resources :foods, only: [:index, :new, :create, :destroy]
+    get "/general_shopping_list", to: "foods#show"
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
