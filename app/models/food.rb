da87c54e4baa6f561f91aa8ef2d468642fbcd479
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-30 23:03:12 -0500

    Fix rubocop issues

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :recipes_foods
-  has_many :recipes :through => :recipes_foods
+  has_many :recipes, through: :recipes_foods
 
   validates :name, length: { maximum: 32 }, allow_blank: false
   validates :measurement_unit, length: { maximum: 8 }, allow_blank: false
