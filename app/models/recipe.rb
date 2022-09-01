57c5cac0da01b94f70de43f4c126ceafcf2b99d6
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-01 18:38:14 +0200

    Fix associations in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User'
-  has_many :recipe_food, dependent: :delete_all
+  has_many :recipe_foods, dependent: :delete_all
   has_many :foods, through: :recipe_foods
 end
