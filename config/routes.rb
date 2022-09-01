1be7f6f1ce22b9162d4b94aa2241d5a505f4a462
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-01 15:40:02 -0500

    Hotfix recipes index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "/foods", to: "foods#index"
   get "/general_shopping_list", to: "foods#list"
   resources :users do
-    resources :recipes, only: [:show, :new, :create, :destroy, :public_recipes]
+    resources :recipes, only: [:index, :show, :new, :create, :destroy, :public_recipes]
     resources :foods, only: [:new, :create, :destroy]
   end
   root "recipes#public_recipes"
