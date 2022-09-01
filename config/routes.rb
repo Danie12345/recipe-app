c7de97dba6949bb1d339961d95f5c6aa2ff1c7df
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-01 15:23:15 -0500

    Added custom index route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/general_shopping_list", to: "foods#list"
   resources :users do
     resources :recipes, only: [:show, :new, :create, :destroy, :public_recipes]
-    resources :foods, only: [:index, :new, :create, :destroy]
+    resources :foods, only: [:new, :create, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
