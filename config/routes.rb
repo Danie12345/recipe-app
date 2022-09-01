dd2de944021312c97dbeabd75b532806e3728028
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-01 15:30:43 -0500

    Update routes
    Change /inventory to /foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get "/inventory", to: "foods#index"
+  get "/foods", to: "foods#index"
   get "/general_shopping_list", to: "foods#list"
   resources :users do
     resources :recipes, only: [:show, :new, :create, :destroy, :public_recipes]
     resources :foods, only: [:new, :create, :destroy]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "recipes#public_recipes"
 end
