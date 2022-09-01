86948ac46652e56fa6ed494dcde8a462740741aa
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-01 08:53:06 -0500

    Revert "Revert "Add food inventory/index route""
    This reverts commit a0402c557f00d5dd37f96d21b3b618121a1403e6.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get "/inventory", to: "foods#index"
   get "/general_shopping_list", to: "foods#list"
   resources :users do
-    resources :recipes, only: [:index, :show, :new, :create, :destroy, :public_recipes]
+    resources :recipes, only: [:show, :new, :create, :destroy, :public_recipes]
     resources :foods, only: [:index, :new, :create, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
