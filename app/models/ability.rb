8afcf5d3ed16a450acf6dd224bec6cdf415be266
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-02 12:21:57 -0500

    Add cancan abilities
    Add abilities to guest
    Add abilities to users
    There are no admins

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    can :read, Recipe, public: true
+
+    return unless user.present?
+
+    can :manage, Recipe, user_id: user.id
+    can :manage, RecipeFood
+    can :manage, Food, user_id: user.id
+  end
+end
