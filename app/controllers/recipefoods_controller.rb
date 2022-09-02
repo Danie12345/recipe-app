0a14e71c454ce3ba2c1899b02538283de6d533b4
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-09-02 02:05:55 -0500

    Add edit and update methods

--- app/controllers/recipefoods_controller.rb
+++ app/controllers/recipefoods_controller.rb
@@ -1,7 +1,6 @@
 class RecipefoodsController < ApplicationController
   def new
     env_vars
-    @recipe = Recipe.find(params[:recipe_id])
     @recipefood = RecipeFood.new
   end
 
@@ -20,6 +19,23 @@ class RecipefoodsController < ApplicationController
     end
   end
 
+  def edit
+    env_vars
+    @recipefood = RecipeFood.find(params[:id])
+    @food = Food.find(@recipefood.food_id)
+  end
+
+  def update
+    env_vars
+    @recipefood = RecipeFood.find(params[:id])
+    @food = Food.find(@recipefood.food_id)
+    if @recipefood.update(edit_params)
+        redirect_to recipe_path(@recipe.id)
+    else
+        render 'edit'
+    end
+  end
+
   def destroy
     @recipefood = RecipeFood.find(params[:id])
     @recipefood.destroy
@@ -29,9 +45,14 @@ class RecipefoodsController < ApplicationController
   def env_vars
     @client = current_user
     @foods = @client.foods
+    @recipe = Recipe.find(params[:recipe_id])
   end
 
   def recipefood_params
     params.permit(:quantity, :food_id, :recipe_id)
+  end
+
+  def edit_params
+    params.require(:recipe_food).permit(:quantity, :food_id, :recipe_id)    
   end
 end
