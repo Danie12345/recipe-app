cf2ae156931d6daad078730b6534f213d6b68f87
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-03 09:43:18 +0200

    Add validation for recipe name

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :recipe_foods, dependent: :delete_all
   has_many :foods, through: :recipe_foods
+
+  validates :name, length: { maximum: 32 }, allow_blank: false
 end
