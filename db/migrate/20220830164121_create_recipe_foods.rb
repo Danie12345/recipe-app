fe2cc8802d2679768baa0c25cffa80853702c15b
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-03 12:59:26 +0200

    Fix recipe_foods migration

--- db/migrate/20220830164121_create_recipe_foods.rb
+++ db/migrate/20220830164121_create_recipe_foods.rb
@@ -1,4 +1,4 @@
-class CreateJoinTableRecipeFoods < ActiveRecord::Migration[7.0]
+class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.decimal :quantity
